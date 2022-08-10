e0e96c1be415be0b3ef60d2dfbe84d206f3c00cd
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-08-10 20:02:57 +0900

    [再提出]課題JavaScript(電卓)[修正点:符号の連続入力を制御]

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 // 表示する値(初期値0)を取得し、変数resultに代入
 let result = document.getElementById('result');
 // ボタンの要素を一括取得
@@ -10,6 +11,36 @@ buttons.forEach((button) => {
     // クリックされたボタンの値を変数pushedValueに代入
     let pushedValue = button.innerHTML;
 
+    // 条件式(表示中の末尾が符号の場合)を、定数symbolに代入
+    const symbol = (
+       result.innerHTML.slice(-1) === '+'
+    || result.innerHTML.slice(-1) === '-'
+    || result.innerHTML.slice(-1) === '*'
+    || result.innerHTML.slice(-1) === '/'
+    || result.innerHTML.slice(-1) === '.')
+
+    // 関数宣言(「0」が先頭にならないように制御)
+    function display() {
+      // 表示中の値が「0」か「00」なら
+      if (result.innerHTML == '0' || result.innerHTML == '00') {
+        // クリックされた値を表示
+        result.innerHTML = pushedValue;
+      } else {
+        // 表示されている値の末尾に追加して表示
+        result.innerHTML += pushedValue;
+      }
+    }
+
+    // 関数宣言(符号ボタンの連続クリックを制御)
+    function symbolOff() {
+      // 表示中の末尾が符号の場合は処理なし
+      if (symbol) {
+        alert('符号は連続で入力できません');
+      } else {
+        display();
+      }
+    }
+
     // クリックされたボタンの値が...
     switch (pushedValue) {
       // 「AC」の場合
@@ -20,21 +51,51 @@ buttons.forEach((button) => {
       
       // 「=」の場合
       case '=':
-        // 表示されている値の計算結果を表示
-        result.innerHTML = eval(result.innerHTML);
+        // 表示中の末尾が符号の場合は処理なし
+        if (symbol) {
+          alert('末尾が符号です')
+          return;
+        } else {
+          // 表示されている値の計算結果を表示
+          result.innerHTML = eval(result.innerHTML);
+        }
+      break;
+
+      // 「+」の場合
+      case '+':
+        symbolOff();
+      break;
+      
+      // 「-」の場合
+      case '-':
+        symbolOff();
+      break;
+
+      // 「*」の場合
+      case '*':
+        symbolOff();
+      break;
+
+      // 「/」の場合
+      case '/':
+        symbolOff();
+      break;
+
+      // 「.」の場合
+      case '.':
+        // 複数入力できないよう制御
+        if (result.innerHTML.includes('.')) {
+          alert('「.」は複数入力できません');
+          return;
+        } else {
+          symbolOff();
+        }
       break;
 
       // それ以外の場合
       default:
-        // 表示されている値が「0」か「00」なら
-        if (result.innerHTML == '0' || result.innerHTML == '00') {
-          // クリックされた値を表示
-          result.innerHTML = pushedValue;}
-        // 表示されている値がそれ以外なら
-        else {
-          // 表示されている値の末尾に追加して表示
-          result.innerHTML += pushedValue;}
+        display();
       break;
-    };
+      }
   });
 });
